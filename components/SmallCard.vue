13091b7dfc9aa57c1781adeb082770b0710c6bba
Faizan Ullah <faizanullah1999@gmail.com>
2022-01-03 20:53:45 +0500

    Added Hover effects to Cards

--- components/SmallCard.vue
+++ components/SmallCard.vue
@@ -1,26 +1,34 @@
 <template>
-    <NuxtLink :to="`/products/${card.id}`" class="card-container">
-        <nuxt-img :src="`/images/${card.image || 'fe1.jpg'}`" alt="fe" class="image" />
-    </NuxtLink>
+  <NuxtLink :to="`/products/${card.id}`" class="card-container">
+    <nuxt-img
+      :src="`/images/${card.image || 'fe1.jpg'}`"
+      alt="fe"
+      class="image"
+    />
+  </NuxtLink>
 </template>
 
 <script>
-    export default {
-        props: ['card']
-    }
+export default {
+  props: ['card'],
+}
 </script>
 
 <style lang="scss" scoped>
 .card-container {
-        width: 24%;
-        height: 12.5rem;
-        border-radius: 0.5rem;
-        overflow: hidden;
-        margin-bottom: 1.5rem;
-        cursor: pointer;
-    }
-    .image {
-        width: 100%;
-        height: 100%
-    }
+  width: 24%;
+  height: 12.5rem;
+  overflow: hidden;
+  margin-bottom: 1.5rem;
+  cursor: pointer;
+  border-radius: 0.5rem;
+  transition: all 0.3s ease;
+  &:hover{
+      transform: scale(1.02);
+  }
+}
+.image {
+  width: 100%;
+  height: 100%;
+}
 </style>
